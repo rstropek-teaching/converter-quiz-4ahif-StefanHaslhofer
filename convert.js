e1ece6fc113e2d9a11772de1bc86aa4139634130
StefanHaslhofer <32057772+StefanHaslhofer@users.noreply.github.com>
2017-09-27 14:54:47 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log('Not implemented yet!');
+var convert = require('convert-units');
+if(process.argv[4]!=='to'||isNaN(process.argv[2])){
+    process.stdout.write("Invalid parameters\n");
+}else{
+    try {
+        process.stdout.write(process.argv[2]+' '+process.argv[3]+' are '+convert(process.argv[2]).from(process.argv[3]).to(process.argv[5])+' '+process.argv[5]+'\n');
+    }catch(err){
+        process.stdout.write("Invalid parameters\n");
+    }
+}
